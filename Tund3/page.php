3d3dd9599f3b19f40e5c1acfbd8fa664d7e570a9
Jaanus Lille <JaanusLille@users.noreply.github.com>
2018-09-25 11:47:24 +0300

    uued näidised

--- Tund3/page.php
+++ Tund3/page.php
@@ -1,15 +1,35 @@
 ﻿<?php
- 
+  //kutsume välja funktsioonide faili
+  require("functions.php");
+  
   $firstName = "Kodanik";
   $lastName = "Tundmatu";
+  $birthMonth = date("m");
+  $monthNamesET = ["jaanuar"];
   //kontrollime kas kasutaja on midagi kirjutanud
   //var_dump($_POST);
   if (isset($_POST["firstName"])){
-	  $firstName = $_POST["firstName"];
+      //$firstName $_POST);
+	  $firstName = test_input($_POST["firstName"]);
   }
   if (isset($_POST["lastName"])){
-	  $firstName = $_POST["lastName"];
+	  $lastName = test_input($_POST["lastName"]);
   }
+  
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  
+  //täiesti mõttetu, harjutamiseks mõeldud funktsioon
+  function fullname(){
+	$GLOBALS["fullName"] = $GLOBALS["firstName"] ." " .$GLOBALS["lastName"];
+	  
+  }
+  $fullname = "";
+  fullname();
 ?>
   
 <!DOCTYPE html>
@@ -34,7 +54,8 @@
   <p>See rida on loodud koduse töö raames, ei pruugi midagi tähendada või mingit eesmärki teenida ning kindlasti ei ole kõige kujutlusvõimerikkam!</p>
   
   <hr>
-  <form method="POST">
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	<label>Eesnimi:</label>
 	<input type="text" name="firstName">
 	<label>Perekonnanimi:</label>
@@ -47,12 +68,13 @@
   <hr>
   <?php
 	if (isset($_POST["firstName"])){
-		echo "<p>Olete elanud järgnevatel aastatel: </p> \n";
+		echo "<p>" .$fullName .", olete elanud järgnevatel aastatel: </p> \n";
 		echo "</ol> \n";
 			for ($i = $_POST["birthYear"]; $i <= date("Y"); $i ++){
 				echo "<li>" .$i ."</li> \n";
 			}
 		echo "</ol> \n";
+	}
   ?>
 
 </body>
---
+++ Tund4/functions.php
@@ -0,0 +1,51 @@
+﻿<?php
+	//laen andmebaasi info
+	require("../../../vp2018config.php");
+	//echo $GLOBALS["serverUsername"];
+	$database = "if18_jaanus_li_1";
+	
+	//anonüümse sõnumi salvestamine
+	function saveAMsg($msg){
+		$notice = "";
+		//serveri ühendus (server, kasutaja, parool, andmebaas
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS[
+		"serverPassword"], $GLOBALS["database"]);
+		//valmistan ette SQL käsu
+		$stmt = $mysqli->prepare("INSERT INTO vpamsg (message) VALUES(?)");
+		echo $mysqli->error;
+		//asendame SQL käsus küsimärgi päris infoga (andmetüüp, andmed ise
+		//s - string; i - integer; d - decimal(murdarv))
+		$stmt->bind_param("s", $msg);
+		if ($stmt->execute()){
+			$notice = 'Sõnum: "' .$msg .'" on salvestatud.';
+		}	else {
+			$notice = "sõnumi salvestamisel tekkis tõrge: " .$stmt->error;
+		}
+		$stmt->close();
+		$mysqli->close();
+		return $notice;
+	}
+	
+	function listallmessages(){
+		 $msgHTML = "";
+		 $mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS[
+		"serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT message FROM vpamsgl");
+		echo $mysqli->error;
+		$stmt->bind_result($msg);
+		$stmt->execute();
+		while($stmt->fetch()){
+			$msgHTML  .= "<p>" .$msg ."</p> \n";
+		}
+		$stmt->close();
+		$mysqli->close();
+		return $msgHTML;
+	}
+	//tekstisisestuse kontroll
+ function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+ }
+?>
